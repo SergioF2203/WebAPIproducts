9d7d11c81cb6ecc67cb2aad94b3fa36ff9072086
SergioF <fs220381@gmail.com>
2019-06-29 21:23:43 +0300

    Implemented Delete endpoint

---
+++ WebApplicationProducts/Domain/Services/Communication/CategoryResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationProducts.Domain.Models;
+
+namespace WebApplicationProducts.Domain.Services.Communication
+{
+    public class CategoryResponse : BaseResponse
+    {
+        public Category Category { get; protected set; }
+
+        private CategoryResponse(bool success, string message, Category category): base(success, message)
+        {
+            Category = category;
+        }
+
+        public CategoryResponse(Category category) : this(true, string.Empty, category) { }
+
+        public CategoryResponse(string message) : this(false, message, null) { }
+    }
+}
